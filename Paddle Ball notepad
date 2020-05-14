5c330881b4354c0afce74cb94eefe59ca8810999
Olson <wayne.olson@austin.k12.mn.us>
2020-05-14 17:21:18 -0500

    Add project files.

---
+++ Paddle Ball notepad
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+      ********************************************
+             Mr. Olson
+             Week 2 Assignment Due April 13th
+             Paddle Ball
+      **********************************************
+    */
+namespace Paddle_Ball
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int point = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+
+            Cursor.Hide();   // this will hide the cursor
+
+            this.FormBorderStyle = FormBorderStyle.None;   // this will remove andy boarder
+            this.TopMost = true;                            // This will bring the form to the front/panel
+            this.Bounds = Screen.PrimaryScreen.Bounds; // this will set the panel as big as the screen
+            Racket.Top = Playground.Bottom - 30;
+
+
+            
+
+
+            lbl_GameOver.Hide();
+            lbl_GameOver.Left = Playground.Width / 2 - lbl_GameOver.Width / 2;  // puts game over in the middle 
+            lbl_GameOver.Top = Playground.Height / 2 - lbl_GameOver.Height / 2; // puts in the top bottom middle
+            
+           
+
+        }
+
+        
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.F1)
+            {
+                point = 0;
+                lbl_points.Text = "0";
+                ball.Top = 30;
+                ball.Left = 30;
+                speed_left = 4;
+                speed_top = 4;
+                timer1.Enabled = true;
+                lbl_GameOver.Hide();
+                Playground.BackColor = Color.White;
+            }
+
+
+            if (e.KeyCode == Keys.P)
+            {
+                if (timer1.Enabled == false)
+                {
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    timer1.Enabled = false;
+                }
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Racket.Left= Cursor.Position.X - (Racket.Width / 2);
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+          
+            // this code checks to see if the ball hits the paddle
+            //  if it does it reverses the speed_top and it continues
+
+            if (ball.Bottom >= Racket.Top && ball.Bottom <= Racket.Bottom && ball.Left >= Racket.Left && ball.Right <= Racket.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;
+
+                point += 1;
+                lbl_points.Text = point.ToString();
+
+                Random r = new Random();
+                Playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+
+            }
+            if (ball.Left <= Playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= Playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <= Playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+
+            // 
+
+            // this code checks to see if it hits the bottom 
+            // if it does the time is stoped and the game is over.
+            if (ball.Bottom >= Playground.Bottom)
+            {
+                timer1.Enabled = false;
+                lbl_GameOver.Show();
+
+            }
+
+        }
+
+        private void Ball_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+}
---
+++ Paddle Ball/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Paddle_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.Racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_points = new System.Windows.Forms.Label();
+            this.lbl_GameOver = new System.Windows.Forms.Label();
+            this.Playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Playground
+            // 
+            this.Playground.Controls.Add(this.lbl_GameOver);
+            this.Playground.Controls.Add(this.lbl_points);
+            this.Playground.Controls.Add(this.label1);
+            this.Playground.Controls.Add(this.ball);
+            this.Playground.Controls.Add(this.Racket);
+            this.Playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Playground.Location = new System.Drawing.Point(0, 0);
+            this.Playground.Name = "Playground";
+            this.Playground.Size = new System.Drawing.Size(800, 450);
+            this.Playground.TabIndex = 0;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(91, 120);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.Ball_Click);
+            // 
+            // Racket
+            // 
+            this.Racket.BackColor = System.Drawing.Color.Black;
+            this.Racket.Location = new System.Drawing.Point(285, 376);
+            this.Racket.Name = "Racket";
+            this.Racket.Size = new System.Drawing.Size(200, 20);
+            this.Racket.TabIndex = 0;
+            this.Racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Score";
+            // 
+            // lbl_points
+            // 
+            this.lbl_points.AutoSize = true;
+            this.lbl_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_points.Location = new System.Drawing.Point(118, 9);
+            this.lbl_points.Name = "lbl_points";
+            this.lbl_points.Size = new System.Drawing.Size(35, 37);
+            this.lbl_points.TabIndex = 7;
+            this.lbl_points.Text = "0";
+            // 
+            // lbl_GameOver
+            // 
+            this.lbl_GameOver.AutoSize = true;
+            this.lbl_GameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_GameOver.Location = new System.Drawing.Point(282, 170);
+            this.lbl_GameOver.Name = "lbl_GameOver";
+            this.lbl_GameOver.Size = new System.Drawing.Size(183, 148);
+            this.lbl_GameOver.TabIndex = 8;
+            this.lbl_GameOver.Text = "Game Over\r\n\r\nF1 - Restart\r\nEsc - Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Playground);
+            this.Name = "Form1";
+            this.Text = "Paddle Ball";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Playground.ResumeLayout(false);
+            this.Playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox Racket;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_GameOver;
+        private System.Windows.Forms.Label lbl_points;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
